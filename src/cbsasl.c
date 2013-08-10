646fda48dbdf7f976d14cb66ef2136882522dd28
Trond Norbye <trond.norbye@gmail.com>
2013-08-10 11:16:15 +0200

    Add visibility tags

--- src/cbsasl.c
+++ src/cbsasl.c
@@ -35,11 +35,13 @@ cbsasl_error_t cbsasl_list_mechs(const char **mechs,
     return SASL_OK;
 }
 
+CBSASL_PUBLIC_API
 cbsasl_error_t cbsasl_init() {
     srand(getpid());
     return load_user_db();
 }
 
+CBSASL_PUBLIC_API
 cbsasl_error_t cbsasl_start(cbsasl_conn_t **conn,
                             const char* mech) {
     cbsasl_error_t err;
@@ -70,6 +72,7 @@ cbsasl_error_t cbsasl_start(cbsasl_conn_t **conn,
     return (*conn)->mech.start(*conn);
 }
 
+CBSASL_PUBLIC_API
 cbsasl_error_t cbsasl_step(cbsasl_conn_t *conn,
                            const char* input,
                            unsigned inputlen,
@@ -78,6 +81,7 @@ cbsasl_error_t cbsasl_step(cbsasl_conn_t *conn,
     return conn->mech.step(conn, input, inputlen, output, outputlen);
 }
 
+CBSASL_PUBLIC_API
 void cbsasl_dispose(cbsasl_conn_t **conn) {
     if (*conn != NULL) {
         free((*conn)->username);
