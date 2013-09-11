0f6aea38d46990548954a77457ccaedf0dd7dbe0
Trond Norbye <trond.norbye@gmail.com>
2013-09-11 05:23:23 -0700

    Add C linkage
    Change-Id: Icdf5602d396d2dc9d2d8bc87c4c0fe8f7dbbf3d2
    Reviewed-on: http://review.couchbase.org/28921
    Tested-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- include/cbsasl/cbsasl.h
+++ include/cbsasl/cbsasl.h
@@ -19,106 +19,116 @@
 
 #include <cbsasl/visibility.h>
 
-typedef enum cbsasl_error {
-    SASL_OK,
-    SASL_CONTINUE,
-    SASL_FAIL,
-    SASL_NOMEM,
-    SASL_BADPARAM,
-    SASL_NOUSER
-} cbsasl_error_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef struct cbsasl_conn_t cbsasl_conn_t;
+    typedef enum cbsasl_error {
+        SASL_OK,
+        SASL_CONTINUE,
+        SASL_FAIL,
+        SASL_NOMEM,
+        SASL_BADPARAM,
+        SASL_NOUSER
+    }
+    cbsasl_error_t;
 
-typedef cbsasl_error_t (*cbsasl_init_fn)();
-typedef cbsasl_error_t (*cbsasl_start_fn)(cbsasl_conn_t *);
-typedef cbsasl_error_t (*cbsasl_step_fn)(cbsasl_conn_t *, const char *,
-                                         unsigned, const char **, unsigned *);
+    typedef struct cbsasl_conn_t cbsasl_conn_t;
 
-typedef struct cbsasl_mechs {
-    const char *name;
-    cbsasl_init_fn init;
-    cbsasl_start_fn start;
-    cbsasl_step_fn step;
-} cbsasl_mechs_t;
+    typedef cbsasl_error_t (*cbsasl_init_fn)();
+    typedef cbsasl_error_t (*cbsasl_start_fn)(cbsasl_conn_t *);
+    typedef cbsasl_error_t (*cbsasl_step_fn)(cbsasl_conn_t *, const char *,
+                                             unsigned, const char **, unsigned *);
 
-struct cbsasl_conn_t {
-    char *username;
-    char *config;
-    char *sasl_data;
-    unsigned sasl_data_len;
-    cbsasl_mechs_t mech;
-};
+    typedef struct cbsasl_mechs {
+        const char *name;
+        cbsasl_init_fn init;
+        cbsasl_start_fn start;
+        cbsasl_step_fn step;
+    } cbsasl_mechs_t;
 
-/**
- * Lists all of the mechanisms this sasl server supports
- *
- * @param mechs A string containing all supported mechanism names
- * @param mechslen The length of the mechs string
- *
- * @return Whether or not an error occured while getting the mechanism list
- */
-CBSASL_PUBLIC_API
-cbsasl_error_t cbsasl_list_mechs(const char **mechs,
-                                 unsigned *mechslen);
+    struct cbsasl_conn_t {
+        char *username;
+        char *config;
+        char *sasl_data;
+        unsigned sasl_data_len;
+        cbsasl_mechs_t mech;
+    };
 
-/**
- * Initializes the sasl server
- *
- * This function initializes the server by loading passwords from the cbsasl
- * password file. This function should only be called once.
- *
- * @return Whether or not the sasl server initialization was successful
- */
-CBSASL_PUBLIC_API
-cbsasl_error_t cbsasl_init(void);
+    /**
+     * Lists all of the mechanisms this sasl server supports
+     *
+     * @param mechs A string containing all supported mechanism names
+     * @param mechslen The length of the mechs string
+     *
+     * @return Whether or not an error occured while getting the mechanism list
+     */
+    CBSASL_PUBLIC_API
+    cbsasl_error_t cbsasl_list_mechs(const char **mechs,
+                                     unsigned *mechslen);
 
-/**
- * Creates a sasl connection and begins authentication
- *
- * When a client receives a request for sasl authentication this function is
- * called in order to initialize the sasl connection based on the mechanism
- * specified.
- *
- * @param conn The connection context for this session
- * @param mechanism The mechanism that will be used for authentication
- *
- * @return Whether or not the mecahnism initialization was successful
- */
-CBSASL_PUBLIC_API
-cbsasl_error_t cbsasl_start(cbsasl_conn_t **conn,
-                            const char *mechanism);
+    /**
+     * Initializes the sasl server
+     *
+     * This function initializes the server by loading passwords from the cbsasl
+     * password file. This function should only be called once.
+     *
+     * @return Whether or not the sasl server initialization was successful
+     */
+    CBSASL_PUBLIC_API
+    cbsasl_error_t cbsasl_init(void);
 
-/**
- * Does username/password authentication
- *
- * After the sasl connection is initialized the step function is called to
- * check credentials.
- *
- * @return Whether or not the sasl step was successful
- */
-CBSASL_PUBLIC_API
-cbsasl_error_t cbsasl_step(cbsasl_conn_t *conn,
-                           const char *input,
-                           unsigned inputlen,
-                           const char **output,
-                           unsigned *outputlen);
+    /**
+     * Creates a sasl connection and begins authentication
+     *
+     * When a client receives a request for sasl authentication this function is
+     * called in order to initialize the sasl connection based on the mechanism
+     * specified.
+     *
+     * @param conn The connection context for this session
+     * @param mechanism The mechanism that will be used for authentication
+     *
+     * @return Whether or not the mecahnism initialization was successful
+     */
+    CBSASL_PUBLIC_API
+    cbsasl_error_t cbsasl_start(cbsasl_conn_t **conn,
+                                const char *mechanism);
 
-/**
- * Frees up funushed sasl connections
- *
- * @param conn The sasl connection to free
- */
-CBSASL_PUBLIC_API
-void cbsasl_dispose(cbsasl_conn_t **pconn);
+    /**
+     * Does username/password authentication
+     *
+     * After the sasl connection is initialized the step function is called to
+     * check credentials.
+     *
+     * @return Whether or not the sasl step was successful
+     */
+    CBSASL_PUBLIC_API
+    cbsasl_error_t cbsasl_step(cbsasl_conn_t *conn,
+                               const char *input,
+                               unsigned inputlen,
+                               const char **output,
+                               unsigned *outputlen);
 
-/**
- * Refresh the internal data (this may result in loading password
- * databases etc)
- *
- * @return Whether or not the operation was successful
- */
-CBSASL_PUBLIC_API
-cbsasl_error_t cbsasl_refresh(void);
+    /**
+     * Frees up funushed sasl connections
+     *
+     * @param conn The sasl connection to free
+     */
+    CBSASL_PUBLIC_API
+    void cbsasl_dispose(cbsasl_conn_t **pconn);
+
+    /**
+     * Refresh the internal data (this may result in loading password
+     * databases etc)
+     *
+     * @return Whether or not the operation was successful
+     */
+    CBSASL_PUBLIC_API
+    cbsasl_error_t cbsasl_refresh(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif  /* INCLUDE_CBSASL_CBSASL_H_ */
